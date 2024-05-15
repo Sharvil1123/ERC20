abd81b955bb5cfa90d5ab5ea124a988b6ab98d6a
Sharvil1123 <sharvilbhalke@gmail.com>
2024-05-15 20:13:37 +0530

    3_complete

--- src/OurToken.sol
+++ src/OurToken.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.18;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract OurToken{
-    
+contract OurToken is ERC20{
+    constructor(uint256 initialSupply) ERC20("SharvilToken", "SHT") {
+        _mint(msg.sender, initialSupply);
+    }
 }
